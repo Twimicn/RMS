fa9ffbf3dd7c39074495e853fd259af20ad7eed1
IvanLuLyf <835498692@qq.com>
2020-04-12 01:10:23 +0800

    Initial Commit

---
+++ src/main/java/show/ginah/rms/controller/IndexController.java
@@ -0,0 +1,14 @@
+package show.ginah.rms.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IndexController {
+    @RequestMapping("/")
+    @ResponseBody
+    public String index() {
+        return "Hello World";
+    }
+}
