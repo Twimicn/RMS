7a3d3f3daddd89e12d48b3d020cf63663be5e09e
IvanLuLyf <835498692@qq.com>
2020-04-19 13:05:44 +0800

    新增PostgreSQL 项目表

--- init.postgre.sql
+++ init.postgre.sql
@@ -19,3 +19,13 @@ comment on table rms_user is '用户表';
 
 insert into rms_user (id, username, password, email, phone, role_id, state)
 values (1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'admin@ginah.show', '13312345678', 1, 1);
+
+create table rms_project
+(
+    id          serial
+        constraint rms_project_pk
+            primary key,
+    name        varchar(50) not null,
+    create_time timestamp,
+    state       integer
+);
