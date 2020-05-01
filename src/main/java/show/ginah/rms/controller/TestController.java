edb90446ef2b8184f437b890612fd5460a0d8b8d
IvanLuLyf <835498692@qq.com>
2020-05-01 21:49:19 +0800

    文件上传测试

---
+++ src/main/java/show/ginah/rms/controller/TestController.java
@@ -0,0 +1,23 @@
+package show.ginah.rms.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import show.ginah.rms.service.FileService;
+
+@Controller
+public class TestController {
+    @Autowired
+    @Qualifier("localFileService")
+    FileService fileService;
+
+    @RequestMapping("/test")
+    @ResponseBody
+    public String test(@RequestParam("file") MultipartFile file) {
+        return fileService.upload(file, "");
+    }
+}
