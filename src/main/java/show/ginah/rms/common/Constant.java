1d8ce7e98693253fc5ce8745b6a30ffbf7fd4122
IvanLuLyf <835498692@qq.com>
2020-05-04 17:48:58 +0800

    增加常量类

---
+++ src/main/java/show/ginah/rms/common/Constant.java
@@ -0,0 +1,7 @@
+package show.ginah.rms.common;
+
+public class Constant {
+    public static final int PROJECT_MEMBER = 0;
+    public static final int PROJECT_LEADER = 1;
+    public static final int PROJECT_SPONSOR = 2;
+}
