7832bcff7cee0af0cff301d638299008894082e0
IvanLuLyf <835498692@qq.com>
2020-05-10 19:20:40 +0800

    添加上传接口

--- src/main/java/show/ginah/rms/controller/ResourceController.java
+++ src/main/java/show/ginah/rms/controller/ResourceController.java
@@ -1,31 +1,34 @@
 package show.ginah.rms.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import show.ginah.rms.annotation.Permission;
 import show.ginah.rms.common.ApiResponse;
 import show.ginah.rms.common.PageData;
+import show.ginah.rms.model.Project;
 import show.ginah.rms.model.Resource;
-import show.ginah.rms.service.FileService;
+import show.ginah.rms.model.User;
+import show.ginah.rms.service.ProjectService;
 import show.ginah.rms.service.ResourceService;
 
-@Controller
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
 @RequestMapping("/api/resource")
 @CrossOrigin
 public class ResourceController {
-    private final FileService fileService;
+    private final ProjectService projectService;
     private final ResourceService resourceService;
 
-    public ResourceController(FileService fileService,
-                              ResourceService resourceService) {
-        this.fileService = fileService;
+    public ResourceController(
+            ProjectService projectService,
+            ResourceService resourceService) {
+        this.projectService = projectService;
         this.resourceService = resourceService;
     }
 
     @Permission("admin")
-    @RequestMapping(value = "/list", method = RequestMethod.POST)
-    @ResponseBody
+    @PostMapping(value = "/list")
     public ApiResponse<PageData<Resource>> apiList(
             @RequestParam(name = "page", required = false, defaultValue = "1") int page,
             @RequestParam(name = "size", required = false, defaultValue = "10") int size) {
@@ -35,16 +38,19 @@ public class ResourceController {
                 .build();
         return ApiResponse.<PageData<Resource>>builder().status(0).msg("ok").data(resourcePageData).build();
     }
-  /*
-    @RequestMapping("/listByProjectId")
-    @ResponseBody
-    public ApiResponse<PageData<Resource>> apiListByProjectId() {
 
-    }*/
-
-    @RequestMapping("/test")
-    @ResponseBody
-    public String test(@RequestParam("file") MultipartFile file) {
-        return fileService.upload(file, "res").toString();
+    @Permission("student")
+    @PostMapping("/upload")
+    public ApiResponse<Void> apiAddRes(
+            HttpServletRequest request,
+            @RequestParam("projectId") long projectId,
+            @RequestParam("file") MultipartFile file) {
+        User user = (User) request.getAttribute("curUser");
+        Project project = projectService.getProjectById(projectId);
+        if (projectService.checkInProject(user.getId(), projectId)
+                || user.getRoleId() == 1) {
+            return resourceService.addRes(file, user, project);
+        }
+        return ApiResponse.<Void>builder().status(2002).msg("权限不足").build();
     }
 }
