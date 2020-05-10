16eb50a467a8dafdf01217a1a4518e627b1f2878
IvanLuLyf <835498692@qq.com>
2020-05-10 19:20:12 +0800

    增加资源上传和获取服务

--- src/main/java/show/ginah/rms/service/ResourceService.java
+++ src/main/java/show/ginah/rms/service/ResourceService.java
@@ -1,7 +1,11 @@
 package show.ginah.rms.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import show.ginah.rms.common.ApiResponse;
+import show.ginah.rms.model.Project;
 import show.ginah.rms.model.Resource;
+import show.ginah.rms.model.User;
 
 import java.util.List;
 
@@ -12,4 +16,8 @@ public interface ResourceService {
     List<Resource> getResourcesByPage(int page, int size);
 
     int count();
+
+    List<Resource> getResourcesByProjectId(long id);
+
+    ApiResponse<Void> addRes(MultipartFile file, User user, Project project);
 }
--- src/main/java/show/ginah/rms/service/impl/ResourceServiceImpl.java
+++ src/main/java/show/ginah/rms/service/impl/ResourceServiceImpl.java
@@ -1,8 +1,14 @@
 package show.ginah.rms.service.impl;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import show.ginah.rms.common.ApiResponse;
 import show.ginah.rms.dao.ResourceDao;
+import show.ginah.rms.model.FileInfo;
+import show.ginah.rms.model.Project;
 import show.ginah.rms.model.Resource;
+import show.ginah.rms.model.User;
+import show.ginah.rms.service.FileService;
 import show.ginah.rms.service.ResourceService;
 
 import java.util.List;
@@ -11,9 +17,11 @@ import java.util.List;
 public class ResourceServiceImpl implements ResourceService {
 
     private ResourceDao resourceDao;
+    private FileService fileService;
 
-    public ResourceServiceImpl(ResourceDao resourceDao) {
+    public ResourceServiceImpl(ResourceDao resourceDao, FileService fileService) {
         this.resourceDao = resourceDao;
+        this.fileService = fileService;
     }
 
     @Override
@@ -23,11 +31,41 @@ public class ResourceServiceImpl implements ResourceService {
 
     @Override
     public List<Resource> getResourcesByPage(int page, int size) {
-        return resourceDao.getResourcesByPage((page - 1) * size, size);
+        List<Resource> resources = resourceDao.getResourcesByPage((page - 1) * size, size);
+        for (Resource resource : resources) {
+            resource.setLink(fileService.pathToUrl(resource.getStorage()));
+        }
+        return resources;
     }
 
     @Override
     public int count() {
         return resourceDao.count();
+    }
+
+    @Override
+    public List<Resource> getResourcesByProjectId(long id) {
+        List<Resource> resources = resourceDao.getResourcesByProjectId(id);
+        for (Resource resource : resources) {
+            resource.setLink(fileService.pathToUrl(resource.getStorage()));
+        }
+        return resources;
+    }
+
+    @Override
+    public ApiResponse<Void> addRes(MultipartFile file, User user, Project project) {
+        if (project == null) {
+            return ApiResponse.<Void>builder().status(3001).msg("项目不存在").build();
+        }
+        FileInfo fileInfo = fileService.upload(file, "res");
+        if (fileInfo.getPath().equals("")) {
+            return ApiResponse.<Void>builder().status(3002).msg("文件上传失败").build();
+        }
+        Resource resource = new Resource(fileInfo.getFilename(), fileInfo.getPath(), user.getId(), project.getId(), user.getName(), project.getName());
+        int r = resourceDao.create(resource);
+        if (r <= 0) {
+            return ApiResponse.<Void>builder().status(-6).msg("数据库出错").build();
+        }
+        return ApiResponse.<Void>builder().status(0).msg("ok").build();
     }
 }
