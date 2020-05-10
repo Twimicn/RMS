0350202ef64e7ca0a2635afd5312349a9cf8d9e0
IvanLuLyf <835498692@qq.com>
2020-05-10 19:15:13 +0800

    新增构造函数和字段

--- src/main/java/show/ginah/rms/model/Resource.java
+++ src/main/java/show/ginah/rms/model/Resource.java
@@ -13,8 +13,23 @@ public class Resource {
     private Date createTime;
     private long userId;
     private long projectId;
-    private int state;
-
     private String userName;
     private String projectName;
+    private int state;
+    private String link;
+
+    public Resource() {
+
+    }
+
+    public Resource(String name, String storage, long userId, long projectId, String userName, String projectName) {
+        this.name = name;
+        this.storage = storage;
+        this.userId = userId;
+        this.projectId = projectId;
+        this.userName = userName;
+        this.projectName = projectName;
+        this.createTime = new Date();
+        this.state = 0;
+    }
 }
